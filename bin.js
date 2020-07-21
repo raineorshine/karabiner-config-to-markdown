c2c3ccecfac9396e41d3471d8c3f17fc56dafb8d
Raine Revere <raine@cybersemics.org>
2020-07-21 14:12:01 -0600

    bin #!

--- bin.js
+++ bin.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const path = require('path')
 const karabinerConfigToMarkdown = require('./index')
 const configPath = path.resolve(process.argv[2] || path.join(process.env.HOME, '/.config/karabiner/karabiner.json'))
